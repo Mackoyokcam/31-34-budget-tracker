3a4c05722f320bff5a63e1207040bc0d1a6337be
mackoyokcam <mark.hangarter@gmail.com>
2017-10-25 21:31:48 -0700

    renderIfs working

--- lab-mark/src/component/expense-item/index.js
+++ lab-mark/src/component/expense-item/index.js
@@ -3,20 +3,37 @@ import React from 'react'
 import {connect} from 'react-redux'
 import ExpenseForm from '../expense-form'
 import * as expense from '../../action/expenses.js'
+import * as util from '../../lib/util.js'
 
 class ExpenseItem extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {editing: false}
+    this.handleUpdate = this.handleUpdate.bind(this)
+  }
+
+  handleUpdate(expense){
+    this.props.expenseUpdate(expense)
+    this.setState({editing: false})
+  }
+
   render(){
     let {
       expense,
       expenseDestroy,
-      expenseUpdate,
     } = this.props
+    let {editing} = this.state
 
     return (
       <div className='expense-item'>
-        <ExpenseForm expense={expense} onComplete={expenseUpdate} />
-        <p> {expense.name} </p>
-        <p> {expense.price} </p>
+        {util.renderIf(editing,
+          <ExpenseForm expense={expense} onComplete={this.handleUpdate} />)}
+        {util.renderIf(!editing,
+          <div onDoubleClick={() => this.setState({editing: true})}>
+            <p> {expense.name} </p>
+            <p> {expense.price} </p>
+          </div>
+        )}
         <button className='delete' onClick={() => expenseDestroy(expense)}>
           Delete
         </button>
